0f10ee0ccb804c6c2cd9af4c0709391514962634
admin <admin@admin-PC>
2019-10-28 14:46:05 +0530

    Security Lab

---
+++ src/securitylab/CeasarCipher.java
@@ -0,0 +1,58 @@
+/*
+Output
+
+Enter the String for Encryption: 
+xyz
+Encryption message= abc
+Decryption message= xyz
+*/
+package securitylab;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author admin
+ */
+public class CeasarCipher {
+
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String encrypt(String plainText, int shiftKey) {
+        plainText = plainText.toLowerCase();
+        String cipherText = "";
+        for (int i = 0; i < plainText.length(); i++) {
+            int charPosition = ALPHABET.indexOf(plainText.charAt(i));
+            int keyVal = (shiftKey + charPosition) % 26;
+            char replaceVal = ALPHABET.charAt(keyVal);
+            cipherText += replaceVal;
+        }
+        return cipherText;
+
+    }
+
+    public static String decrypt(String cipherText, int shiftKey) {
+        cipherText = cipherText.toLowerCase();
+        String plainText = "";
+        for (int i = 0; i < cipherText.length(); i++) {
+            int charPosition = ALPHABET.indexOf(cipherText.charAt(i));
+            int keyVal = (charPosition - shiftKey) % 26;
+            if (keyVal < 0) {
+                keyVal = ALPHABET.length() + keyVal;
+            }
+            char replaceVal = ALPHABET.charAt(keyVal);
+            plainText += replaceVal;
+        }
+        return plainText;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String for Encryption: ");
+        String message = new String();
+        message = sc.next();
+        System.out.println("Encryption message= " + encrypt(message, 3));
+        System.out.println("Decryption message= " + decrypt(encrypt(message, 3), 3));
+        sc.close();
+    }
+
+}
